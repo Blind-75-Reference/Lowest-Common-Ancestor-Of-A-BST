53ec7c62fa99feb51c92042b0a42722e6b7e273d
LiquidPlummer <kyle.plummer@revature.com>
2023-03-23 12:00:34 -0400

    why?

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
+    /*
+    Brute force solution - This two-step solution is fairly straightforward. First traverse the tree
+    to find p and q tracking your traversal so the route can be analyzed. Then consider both traversals
+    looking for the lowest common ancestor.
+
+    solution unfinished. Do I want to actually do bad solutions? It's becoming more difficult to do the bad solutions
+    than the good ones.
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        //Write your solution here...
+
 
         return null;
+    }
+
+    private List<Integer> recursiveSearch(TreeNode root, int target, List<Integer> traversalList) {
+        if(root == null) {
+            return traversalList;
+        } else if(root.val == target) {
+                traversalList.add(root.val);
+                return traversalList;
+            }
+        }
+
+
     }
 }
